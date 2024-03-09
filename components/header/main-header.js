41e43e8709553b8685276b0bb4cc44fdc175695c
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-03-08 20:31:50 -0500

    feat: add header and its styles

---
+++ components/header/main-header.js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+import logo from '@/public/images/logo.png';
+import Link from 'next/link';
+import styles from './header.module.css';
+
+function Header() {
+  return (
+    <div className={styles['header-container']}>
+      <Link href="/" className={styles['header-menu']}>
+        <Image src={logo} alt="logo" />
+        <p>NEXTLEVEL FOOD</p>
+      </Link>
+      <div className={styles['header-menu']}>
+        <Link className={styles.link} href="/meals">
+          Browse Meals
+        </Link>
+        <Link className={styles.link} href="/community">
+          Foodies Community
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
