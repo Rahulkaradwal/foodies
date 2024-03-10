348d54c96e5e5723fe0394ac66cd519a15a793be
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-03-10 14:32:38 -0400

    feat: add meal detail slug component

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import styles from './page.module.css';
+import { getMeal } from '@/dataRequest/meals';
+import Image from 'next/image';
+function MealDetails({ params }) {
+  // by defulat next sends params to the components
 
-function MealDetails() {
-  return <></>;
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
 export default MealDetails;
--- dataRequest/meals.js
+++ dataRequest/meals.js
@@ -5,3 +5,7 @@ const db = sql('meals.db');
 export function getMeals() {
   return db.prepare('Select * from meals').all();
 }
+
+export function getMeal(slug) {
+  return db.prepare('Select * from meals where slug = ?').get(slug);
+}
