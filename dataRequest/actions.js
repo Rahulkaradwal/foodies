30375bd9fa99fd16e6d683e2d4508216a1c8a202
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-03-11 15:06:31 -0400

    refactor: throw custom errors with userFormState

--- dataRequest/actions.js
+++ dataRequest/actions.js
@@ -3,7 +3,11 @@
 import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
 
-export async function shareMeal(formData) {
+function validate(val) {
+  return !val || val.trim() === '';
+}
+
+export async function shareMeal(preValue, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -12,6 +16,20 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if (
+    validate(meal.title) ||
+    validate(meal.summary) ||
+    validate(meal.instructions) ||
+    validate(meal.creator) ||
+    validate(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return { message: 'Invalid or Empty Input fields' };
+  }
+
   await saveMeal(meal);
   redirect('/meals');
 }
